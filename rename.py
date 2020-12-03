6d4b732452504cd1cbfc2051b441b84a15f35fd6
KomoreKalu <komorekalu@outlook.com>
2020-12-03 22:12:27 +0800

    Add rename.py

---
+++ rename.py
@@ -0,0 +1,32 @@
+import os
+
+video_ext = "mkv"
+sub_ext = "ass"
+folder = os.getcwd()
+video_name = []
+sub_name = []
+
+
+def get_name(path):
+    global video_ext, sub_ext
+    for file in os.listdir(path):
+        if os.path.isfile(file) and video_ext == os.path.splitext(file)[-1][1:]:
+            video_name.append(os.path.splitext(file)[0])
+        if os.path.isfile(file) and sub_ext == os.path.splitext(file)[-1][1:]:
+            sub_name.append(file)
+    if len(video_name) != len(sub_name):
+        print("file amount(video&subtitle) doesn't match")
+        exit(1)
+    video_name.sort()
+    sub_name.sort()
+
+
+def rename():
+    global video_name, sub_name
+    for i in range(len(video_name)):
+        os.rename(sub_name[i], video_name[i] + "." + sub_ext)
+
+
+if __name__ == "__main__":
+    get_name(folder)
+    rename()
